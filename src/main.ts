043b03d0c01e98eb863f7e6d451d25a132e9e56f
b22davbe <129262690+b22davbe@users.noreply.github.com>
2025-04-28 17:38:52 +0200

    Added the .splat

--- src/main.ts
+++ src/main.ts
@@ -8,24 +8,17 @@ viewer.ready.then(() => {
   // Fly till Skara Domkyrka med de önskade koordinaterna och orienteringen
   viewer.flyTo(13.43450246, 58.38425682, 407.51, 46.79851, -33.11150, 2);
 
-  // Ladda in Opera House-lagret (för verifiering)
-  function loadOperaHouse() {
-    const operaHouseLayer = new GaussianSplatLayer(
-      "./data/Sydney.splat",  // Vägen till modellen
+  function addChurch(viewer: Viewer, lat: number, lon: number, height: number) {
+    const churchLayer = new GaussianSplatLayer(
+      "./data/Domkyrkan.splat",
+      { lat, lon, height },
       {
-        lon: 151.21519000000004,
-        lat: -33.859195000000696,
-        height: -16,
+        x: 1.589647099820901,
+        y: 0.2756564998150081,
+        z: 2.5602732916424125,
       },
-      {
-        x: 2.1650596257576185,
-        y: -0.04732982932177055,
-        z: -0.8175728862675601,
-      },
-      91.15
+      27.05 // scale
     );
-    viewer.addGaussianSplatLayer(operaHouseLayer);
+    viewer.addGaussianSplatLayer(churchLayer);
   }
-
-  loadOperaHouse();
 });
